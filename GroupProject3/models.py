e6a7ea4a9987844ea5dbffac9cdbe19a15228f34
kvngard <kev.ngardner@gmail.com>
2015-11-21 11:49:57 -0800

    Created simple models file.

--- GroupProject3/models.py
+++ GroupProject3/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.localflavor.fr.forms import us
 
-# Create your models here.
+
+class location(models.Model):
+    name = models.CharField(max_length=50)
+    office_number = models.IntegerField()
+
+
+class manufacturer(models.Model):
+    name = models.CharField(max_length=50)
+    hq_state = us.models.USStateField
+    contact_phone = us.models.PhoneNumberField
+
+
+class part(models.Model):
+    name = models.CharField(max_length=50)
+    manufacturer = models.ForeignKey(manufacturer)
+
+
+class organization(models.Model):
+    name = models.CharField(max_length=100)
+    contact_phone = us.models.PhoneNumberField
+
+
+class asset(models.Model):
+    location = models.ForeignKey(location)
+    organization = models.ForeignKey(organization)
+    manufacturer = models.ForeignKey(manufacturer)
+    part = models.ForeignKey(manufacturer)
+    description = models.CharField(max_length=400)
+    implemented = models.DateField(auto_now=True)
+    notes = models.CharField(max_length=400)
