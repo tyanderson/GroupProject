32c9d8baf3105e843b0bd2d90832e5e2b26284ec
Ty <ty.anderson.3@gmail.com>
2015-11-21 13:24:31 -0700

    bugfix

--- GroupProject3/views.py
+++ GroupProject3/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Static text')
+    template = loader.get_template('templates/index.html')
+    return render(request, 'polls/index.html')
+    # return HttpResponse(template.render())
